26843265cbdcc4334bd1ab0b42fa7ca319c7be40
Juan Domingo Peron <94873456+Peron4TheWin@users.noreply.github.com>
2022-10-21 18:39:59 -0300

    Update memory fixer.py

--- memory fixer.py
+++ memory fixer.py
@@ -1,17 +1,11 @@
-import psutil
-import os
-
-memoria = psutil.virtual_memory()
-total = memoria.total
-RAM = int(total / 1024 / 1024)
-os.system('bcdedit/set nx Optin')
-
-if RAM > 16341:
-    RAM = 16341
-
-RAM = int(RAM)
-os.system('wmic pagefile list /format:list')
-os.system('wmic computersystem where name="%computername%" set AutomaticManagedPagefile=false')
-pre = r'C:\\pagefile.sys'
-comando = f'wmic pagefileset where name="{pre}" set InitialSize={RAM},MaximumSize={RAM}'
-os.system(comando)
+import subprocess
+def cmd(command):
+    return subprocess.check_output(command, shell=True).decode('utf-8')
+ram=cmd('wmic ComputerSystem get TotalPhysicalMemory | findstr /V "TotalPhysicalMemory"')
+ram=int(int(ram)/1024/1024)
+if ram > 16341:
+    ram = 16341
+ram = int(ram)
+subprocess.run('wmic pagefile list /format:list')
+subprocess.run(r'wmic computersystem where name="%computername%" set AutomaticManagedPagefile=false')
+subprocess.run(fr'wmic pagefileset where name="C:\pagefile.sys" set InitialSize={ram},MaximumSize={ram}')
